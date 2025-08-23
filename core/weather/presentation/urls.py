aa2a0653e67af025135623d827159979013f34e1
VitorAndreSilva <alunovitorandre@gmail.com>
2025-08-23 12:14:34 -0300

    feat: add WeatherViewSet in the weather urls

--- core/weather/presentation/urls.py
+++ core/weather/presentation/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import WeatherSearch
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.weather.presentation.views import WeatherSearch, WeatherViewSet
+
+router = DefaultRouter()
+router.register(r"weahter", WeatherViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("weather/search/", WeatherSearch.as_view(), name="weather-search"),
 ]
