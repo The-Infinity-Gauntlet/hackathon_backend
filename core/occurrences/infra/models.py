042470bb610222e795f5d1fb85d5b5fbc44212e8
VitorAndreSilva <alunovitorandre@gmail.com>
2025-08-23 16:20:40 -0300

    fix: choice option in the situation field in the occurrences app

--- core/occurrences/infra/models.py
+++ core/occurrences/infra/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 class Occurrence(models.Model):
     datetime = models.DateTimeField()
-    situation = models.CharField()
-    type = models.CharField()
-    neighborhood = models.CharField()
+    class Situation(models.IntegerChoices):
+        ALERTA = 1, "alerta"
+        ATENCAO = 2, "atencao"
+        MOBILIZACAO = 3, "mobilizacao"
+        NORMALIDADE = 4, "normalidade"
+    situation = models.IntegerField(choices=Situation.choices, default=Situation.NORMALIDADE)
+    type = models.CharField(max_length=80)
+    neighborhood = models.CharField(max_length=22)
 
     def __str__(self):
         return f'{self.situation} - {self.datetime} - {self.neighborhood}'
