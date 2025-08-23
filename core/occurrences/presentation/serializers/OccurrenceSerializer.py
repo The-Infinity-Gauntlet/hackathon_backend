c3c94e35b5694d6653c561458ba15778fc7bb670
VitorAndreSilva <alunovitorandre@gmail.com>
2025-08-23 16:22:37 -0300

    fix: choice option in the situation field in the occurrences app

--- core/occurrences/presentation/serializers/OccurrenceSerializer.py
+++ core/occurrences/presentation/serializers/OccurrenceSerializer.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 class OccurrenceSerializer(serializers.Serializer):
     datetime = serializers.DateTimeField()
-    situation = serializers.CharField()
+    situation = serializers.CharField(source='get_situation_display')
     type = serializers.CharField()
     neighborhood = serializers.CharField()
-    
